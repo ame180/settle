eaf0bd034c5ffd31e40318259d9dc4bd0ab25c97
Ame <37273244+ame180@users.noreply.github.com>
2025-02-08 21:59:31 +0700

    Add UserCreateCommand to create new Users through CLI

---
+++ src/Command/UserCreateCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:user:create',
+    description: 'Creates a new User',
+)]
+final class UserCreateCommand extends Command
+{
+    public function __construct(
+		private readonly EntityManagerInterface $entityManager,
+		private readonly UserRepository $userRepository,
+		private readonly UserPasswordHasherInterface $passwordHasher,
+	) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+			->addArgument('email', InputArgument::REQUIRED, 'User email')
+			->addArgument('password', InputArgument::REQUIRED, 'User password');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+		$password = $input->getArgument('password');
+
+		if (empty($email) || empty($password))
+		{
+			$io->error('Email and password cannot be empty');
+
+			return Command::FAILURE;
+		}
+
+		$user = $this->userRepository->findOneBy(['email' => $email]);
+		if (null !== $user)
+		{
+			$io->error('User already exists');
+
+			return Command::FAILURE;
+		}
+
+		$user = new User();
+		$user->setEmail($email);
+		$user->setPassword($this->passwordHasher->hashPassword($user, $password));
+
+		$this->entityManager->persist($user);
+		$this->entityManager->flush();
+
+		$io->success('User created successfully');
+
+        return Command::SUCCESS;
+    }
+}
