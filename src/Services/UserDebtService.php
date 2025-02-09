94465bc9518dafc5a19317cde1cad6c068b75f18
Paweł Jacyno <26870368+Pjacyn@users.noreply.github.com>
2025-02-09 17:05:48 +0100

    feat: UserDebt simple service.

---
+++ src/Services/UserDebtService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\User;
+
+class UserDebtService
+{
+    public function getUserDebtAmount(User $user): string
+    {
+        $debtAmount = 0;
+        foreach ($user->getDebts() as $debt) {
+            $debtAmount = bcadd($debt->getAmount(), $debtAmount, 2);
+        }
+        return $debtAmount;
+    }
+}
